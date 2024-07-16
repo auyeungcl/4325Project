413867d423d4f732ce26117bcaac02fccf20e57e
auyeungcl <101610006+auyeungcl@users.noreply.github.com>
2024-07-16 16:19:25 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getAuth, signOut, initializeAuth, getReactNativePersistence } from 'fir
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBrKfelTCnvsoh-RAmVADQ6EHUlczSVOgQ",
-    authDomain: "medease-4e34d.firebaseapp.com",
-    projectId: "medease-4e34d",
-    storageBucket: "medease-4e34d.appspot.com",
-    messagingSenderId: "505144716831",
-    appId: "1:505144716831:web:d3598d719af59e5417ef2a"
+    apiKey: "AIzaSyABfofP4DN-x4zinS5FKQ6j6VE7lnG0l4I",
+    authDomain: "medease-e53f8.firebaseapp.com",
+    projectId: "medease-e53f8",
+    storageBucket: "medease-e53f8.appspot.com",
+    messagingSenderId: "318560143351",
+    appId: "1:318560143351:web:d5a90fb4d593f02f0850f2"
 };
 
 // Ensure Firebase is initialized only once
@@ -20,6 +20,7 @@ const db = getFirestore(app);
 const auth = initializeAuth(app, {
     persistence: getReactNativePersistence(AsyncStorage)
 });
+
 
 
 export { db, auth, app };
