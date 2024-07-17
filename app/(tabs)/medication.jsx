6ba0f780fa3d937ee79cec9443ee35a7841de09b
auyeungcl <101610006+auyeungcl@users.noreply.github.com>
2024-07-16 21:29:06 -0700

    Update medication.jsx

--- app/(tabs)/medication.jsx
+++ app/(tabs)/medication.jsx
@@ -255,7 +255,7 @@ const styles = StyleSheet.create({
   addButton: {
     backgroundColor: Colors.PRIMARY,
     padding: 10,
-    borderRadius: 5,
+    borderRadius: 10,
     margin: 10,
   },
 });
